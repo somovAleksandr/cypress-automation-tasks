c738250fb1f15e7d61a9d7e5404b649c3949b558
somovAleksandr <alexander-199292@yandex.ru>
2026-09-06 07:16:47 +0300

    Add Smart Table user update test

--- cypress/e2e/exams/daily-exam-04.cy.js
+++ cypress/e2e/exams/daily-exam-04.cy.js
@@ -244,4 +244,36 @@ describe("Daily Exam #4", () => {
       });
     });
   });
+
+  it("Should update Larry user data", () => {
+    cy.contains("Tables & Data").click();
+    cy.contains("Smart Table").click();
+
+    const updatedData = {
+      "First Name": "Lawrence",
+      Age: "35",
+    };
+
+    cy.contains("tbody tr", "Larry").within(() => {
+      cy.get(".nb-edit").click();
+    });
+
+    cy.get(".nb-checkmark")
+      .closest("tr")
+      .within(() => {
+        for (const [key, value] of Object.entries(updatedData)) {
+          cy.get(`input[placeholder="${key}"]`)
+            .clear()
+            .type(value)
+            .should("have.value", value);
+        }
+
+        cy.get(".nb-checkmark").click();
+      });
+
+    cy.contains("tbody tr", updatedData["First Name"]).within(() => {
+      cy.get("td").eq(2).should("have.text", updatedData["First Name"]);
+      cy.get("td").last().should("have.text", updatedData.Age);
+    });
+  });
 });
