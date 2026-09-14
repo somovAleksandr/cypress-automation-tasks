ce60447786c0c4c6d246a737f1c54f7e482e9cb7
somovAleksandr <alexander-199292@yandex.ru>
2026-09-14 07:48:19 +0300

    Add Smart Table CRUD flow test

--- cypress/e2e/exams/daily-exam-06.cy.js
+++ cypress/e2e/exams/daily-exam-06.cy.js
@@ -46,4 +46,115 @@ describe("Daily Exam #6", () => {
       cy.contains("button", "Sign in").should("be.visible").and("be.enabled");
     });
   });
+
+  it("Should fill and submit Inline form from using object data", () => {
+    cy.contains("Forms").click();
+    cy.contains("Form Layouts").click();
+
+    const userData = {
+      "Jane Doe": "Robert QA",
+      Email: "robert.qa@test.com",
+    };
+
+    cy.contains("nb-card", "Inline form").within(() => {
+      for (const [key, value] of Object.entries(userData)) {
+        cy.get(`input[placeholder="${key}"]`)
+          .type(value)
+          .should("have.value", value);
+      }
+
+      cy.contains("Remember me")
+        .closest("nb-checkbox")
+        .find('input[type="checkbox"]')
+        .check({ force: true })
+        .should("be.checked");
+
+      cy.contains("button", "Submit")
+        .should("be.visible")
+        .and("be.enabled")
+        .click();
+
+      for (const [key, value] of Object.entries(userData)) {
+        cy.get(`input[placeholder="${key}"]`).should("have.value", value);
+      }
+    });
+  });
+
+  it("Should create, update and detele a  user using object data", () => {
+    cy.contains("Tables & Data").click();
+    cy.contains("Smart Table").click();
+
+    const userData = {
+      "First Name": "Vera",
+      "Last Name": "Koroleva",
+      Username: "VeraQueen",
+      "E-mail": "vera@gmail.com",
+      Age: "10",
+    };
+
+    const updatedData = {
+      "First Name": "Verusha",
+      Age: "11",
+    };
+
+    const values = Object.values(userData);
+
+    cy.get("thead").within(() => {
+      cy.get(".nb-plus").click();
+    });
+
+    cy.get(".nb-checkmark")
+      .closest("tr")
+      .within(() => {
+        for (const [key, value] of Object.entries(userData)) {
+          cy.get(`input[placeholder="${key}"]`)
+            .type(value)
+            .should("have.value", value);
+        }
+
+        cy.get(".nb-checkmark").click();
+      });
+
+    cy.contains("tbody tr", userData["E-mail"]).within(() => {
+      cy.get("td").each(($td, index) => {
+        if (index > 1) {
+          cy.wrap($td).should("have.text", values[index - 2]);
+        }
+      });
+    });
+
+    cy.contains("tbody tr", userData["E-mail"]).within(() => {
+      cy.get(".nb-edit").click();
+    });
+
+    cy.get(".nb-checkmark")
+      .closest("tr")
+      .within(() => {
+        for (const [key, value] of Object.entries(updatedData)) {
+          cy.get(`input[placeholder="${key}"]`)
+            .clear()
+            .type(value)
+            .should("have.value", value);
+        }
+
+        cy.get(".nb-checkmark").click();
+      });
+
+    cy.contains("tbody tr", userData["E-mail"]).within(() => {
+      cy.get("td").eq(2).should("have.text", updatedData["First Name"]);
+      cy.get("td").last().should("have.text", updatedData.Age);
+    });
+
+    cy.window().then((win) => {
+      cy.stub(win, "confirm").as("dialog").returns(true);
+    });
+
+    cy.contains("tbody tr", userData["E-mail"]).within(() => {
+      cy.get(".nb-trash").click();
+    });
+
+    cy.get("@dialog").should("be.called");
+
+    cy.contains("tbody tr", userData["E-mail"]).should("not.exist");
+  });
 });
