7cc5dee6cc7a05b15ed51abe0c615e196ba4c423
somovAleksandr <alexander-199292@yandex.ru>
2026-09-21 07:06:23 +0300

    Add Using the Grid interaction test

---
+++ cypress/e2e/exams/daily-exam-09.cy.js
@@ -0,0 +1,60 @@
+/// <reference types="cypress"/>
+
+function getInputByLabel(label) {
+  return cy.root().contains(label).closest(".form-group").find("input");
+}
+
+function getRadioByLabel(label) {
+  return cy
+    .root()
+    .contains(label)
+    .closest("nb-radio")
+    .find('input[type="radio"]');
+}
+
+describe("Daily Exam #9", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("Should fill Using the Grid form", () => {
+    cy.contains("Forms").click();
+    cy.contains("Form Layouts").click();
+
+    cy.contains("nb-card", "Using the Grid").should("exist").and("be.visible");
+
+    cy.contains("nb-card", "Using the Grid").within(() => {
+      getInputByLabel("Email")
+        .should("have.value", "")
+        .type("exam9@test.com")
+        .should("have.value", "exam9@test.com");
+
+      getInputByLabel("Password")
+        .should("have.value", "")
+        .type("Cypress999")
+        .should("have.value", "Cypress999");
+
+      getRadioByLabel("Option 1")
+        .should("be.enabled")
+        .and("not.be.checked")
+        .check({ force: true })
+        .should("be.checked");
+
+      getRadioByLabel("Option 2").should("be.enabled").and("not.be.checked");
+
+      getRadioByLabel("Disabled Option")
+        .should("be.disabled")
+        .and("not.be.checked");
+
+      getRadioByLabel("Option 2")
+        .should("be.enabled")
+        .and("not.be.checked")
+        .check({ force: true })
+        .should("be.checked");
+
+      getRadioByLabel("Option 1").should("be.enabled").and("not.be.checked");
+
+      cy.contains("button", "Sign in").should("be.visible").and("be.enabled");
+    });
+  });
+});
